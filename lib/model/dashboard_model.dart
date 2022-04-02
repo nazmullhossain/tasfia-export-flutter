72db023fcd4610129750319c38ef363e678d472b
sukhtarasujit <sujit.sukhtara@gmail.com>
2022-04-02 12:22:40 +0600

    02-04-22

--- lib/model/dashboard_model.dart
+++ lib/model/dashboard_model.dart
@@ -1,24 +1,24 @@
 import 'dart:convert';
 
-DashboardModel dashboardModelFromJson(String str) => DashboardModel.fromJson(json.decode(str));
-
-String dashboardModelToJson(DashboardModel data) => json.encode(data.toJson());
+DashboardModel dashboardModelFromJson(String str) =>
+    DashboardModel.fromJson(json.decode(str));
 
 class DashboardModel {
-  DashboardModel({
-    this.todayTotalSalesQuantity,
-    this.todaySalesAmount,
-    this.currentMonthTotalSalesQuantity,
-    this.currentMonthSalesAmount,
-    this.todayPurchaseQuantity,
-    this.todayPurchaseAmount,
-    this.currentMonthPurchaseQuantity,
-    this.currentMonthPurchaseAmount,
-    this.todayDueAmount,
-    this.todayDueQuantity,
-    this.currentMonthDueAmount,
-    this.currentMonthDueQuantity,
-  });
+  DashboardModel(
+      {this.todayTotalSalesQuantity,
+      this.todaySalesAmount,
+      this.currentMonthTotalSalesQuantity,
+      this.currentMonthSalesAmount,
+      this.todayPurchaseQuantity,
+      this.todayPurchaseAmount,
+      this.currentMonthPurchaseQuantity,
+      this.currentMonthPurchaseAmount,
+      this.todayDueAmount,
+      this.todayDueQuantity,
+      this.currentMonthDueAmount,
+      this.currentMonthDueQuantity,
+      this.totalCusdtomer,
+      this.totalSupplier});
 
   final dynamic todayTotalSalesQuantity;
   final dynamic todaySalesAmount;
@@ -32,34 +32,24 @@ class DashboardModel {
   final dynamic todayDueQuantity;
   final dynamic currentMonthDueAmount;
   final dynamic currentMonthDueQuantity;
+  final dynamic totalCusdtomer;
+  final dynamic totalSupplier;
 
   factory DashboardModel.fromJson(Map<String, dynamic> json) => DashboardModel(
-    todayTotalSalesQuantity: json["today_total_sales_quantity"],
-    todaySalesAmount: json["today_sales_amount"],
-    currentMonthTotalSalesQuantity: json["current_month_total_sales_quantity"],
-    currentMonthSalesAmount: json["current_month_sales_amount"],
-    todayPurchaseQuantity: json["today_purchase_quantity"],
-    todayPurchaseAmount: json["today_purchase_amount"],
-    currentMonthPurchaseQuantity: json["current_month_purchase_quantity"],
-    currentMonthPurchaseAmount: json["current_month_purchase_amount"],
-    todayDueAmount: json["today_due_amount"],
-    todayDueQuantity: json["today_due_quantity"],
-    currentMonthDueAmount: json["current_month_due_amount"],
-    currentMonthDueQuantity: json["current_month_due_quantity"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "today_total_sales_quantity": todayTotalSalesQuantity,
-    "today_sales_amount": todaySalesAmount,
-    "current_month_total_sales_quantity": currentMonthTotalSalesQuantity,
-    "current_month_sales_amount": currentMonthSalesAmount,
-    "today_purchase_quantity": todayPurchaseQuantity,
-    "today_purchase_amount": todayPurchaseAmount,
-    "current_month_purchase_quantity": currentMonthPurchaseQuantity,
-    "current_month_purchase_amount": currentMonthPurchaseAmount,
-    "today_due_amount": todayDueAmount,
-    "today_due_quantity": todayDueQuantity,
-    "current_month_due_amount": currentMonthDueAmount,
-    "current_month_due_quantity": currentMonthDueQuantity,
-  };
+        todayTotalSalesQuantity: json["today_total_sales_quantity"],
+        todaySalesAmount: json["today_sales_amount"],
+        currentMonthTotalSalesQuantity:
+            json["current_month_total_sales_quantity"],
+        currentMonthSalesAmount: json["current_month_sales_amount"],
+        todayPurchaseQuantity: json["today_purchase_quantity"],
+        todayPurchaseAmount: json["today_purchase_amount"],
+        currentMonthPurchaseQuantity: json["current_month_purchase_quantity"],
+        currentMonthPurchaseAmount: json["current_month_purchase_amount"],
+        todayDueAmount: json["today_due_amount"],
+        todayDueQuantity: json["today_due_quantity"],
+        currentMonthDueAmount: json["current_month_due_amount"],
+        currentMonthDueQuantity: json["current_month_due_quantity"],
+        totalCusdtomer: json["total_customer"],
+        totalSupplier: json["total_supplier"],
+      );
 }
--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -18,106 +18,140 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-
   List<HomeMenuModel> _dashboardDataList = HomeMenuModel.dashboardDataList;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _initData(PublicController.pc);
   }
 
-  Future<void> _initData(PublicController pc)async{
-    if(PublicController.pc.customerModel.value.data==null
-        ||PublicController.pc.customerModel.value.data!.isEmpty){
+  Future<void> _initData(PublicController pc) async {
+    if (PublicController.pc.customerModel.value.data == null ||
+        PublicController.pc.customerModel.value.data!.isEmpty) {
       await PublicController.pc.getAllCustomer();
     }
-    if(PublicController.pc.supplierModel.value.data==null
-        ||PublicController.pc.supplierModel.value.data!.isEmpty){
+    if (PublicController.pc.supplierModel.value.data == null ||
+        PublicController.pc.supplierModel.value.data!.isEmpty) {
       await PublicController.pc.getAllSupplier();
     }
     _dashboardDataList = [
-      HomeMenuModel(title: 'আজকের মোট বিক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayTotalSalesQuantity}কেজি/${pc.dashboardModel.value.todaySalesAmount}টাকা', icon: LineAwesomeIcons.dollar_sign, color: const Color(0xff007BFF)),
-      HomeMenuModel(title: 'বর্তমান মাসের মোট বিক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthTotalSalesQuantity}কেজি/${pc.dashboardModel.value.currentMonthSalesAmount}টাকা', icon: LineAwesomeIcons.dollar_sign, color: const Color(0xff11CDEF)),
-      HomeMenuModel(title: 'আজকের মোট ক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayPurchaseQuantity}কেজি/${pc.dashboardModel.value.todayPurchaseAmount}টাকা', icon: LineAwesomeIcons.shopping_cart, color: const Color(0xff2DCE89)),
-      HomeMenuModel(title: 'বর্তমান মাসের মোট ক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthPurchaseQuantity}কেজি/${pc.dashboardModel.value.currentMonthPurchaseAmount}টাকা', icon: LineAwesomeIcons.shopping_cart, color: const Color(0xff343A40)),
-      HomeMenuModel(title: 'আজকের মোট বকেয়া\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayDueQuantity}কেজি/${pc.dashboardModel.value.todayDueAmount}টাকা', icon: LineAwesomeIcons.dollar_sign, color: const Color(0xffDF2DE3)),
-      HomeMenuModel(title: 'বর্তমান মাসের মোট পাওনা\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthDueQuantity}কেজি/${pc.dashboardModel.value.currentMonthDueAmount}টাকা', icon: LineAwesomeIcons.dollar_sign, color: const Color(0xffFB6340)),
-      HomeMenuModel(title: 'মোট কাস্টমার\n${pc.customerModel.value.data!.length}', icon: LineAwesomeIcons.users, color: const Color(0xffF5365C)),
-      HomeMenuModel(title: 'মোট সাপ্লায়ার\n${pc.supplierModel.value.data!.length}', icon: LineAwesomeIcons.user_plus, color: const Color(0xffCAD900))];
+      HomeMenuModel(
+          title:
+              'আজকের মোট বিক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayTotalSalesQuantity}কেজি/${pc.dashboardModel.value.todaySalesAmount}টাকা',
+          icon: LineAwesomeIcons.dollar_sign,
+          color: const Color(0xff007BFF)),
+      HomeMenuModel(
+          title:
+              'বর্তমান মাসের মোট বিক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthTotalSalesQuantity}কেজি/${pc.dashboardModel.value.currentMonthSalesAmount}টাকা',
+          icon: LineAwesomeIcons.dollar_sign,
+          color: const Color(0xff11CDEF)),
+      HomeMenuModel(
+          title:
+              'আজকের মোট ক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayPurchaseQuantity}কেজি/${pc.dashboardModel.value.todayPurchaseAmount}টাকা',
+          icon: LineAwesomeIcons.shopping_cart,
+          color: const Color(0xff2DCE89)),
+      HomeMenuModel(
+          title:
+              'বর্তমান মাসের মোট ক্রয়\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthPurchaseQuantity}কেজি/${pc.dashboardModel.value.currentMonthPurchaseAmount}টাকা',
+          icon: LineAwesomeIcons.shopping_cart,
+          color: const Color(0xff343A40)),
+      HomeMenuModel(
+          title:
+              'আজকের মোট বকেয়া\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.todayDueQuantity}কেজি/${pc.dashboardModel.value.todayDueAmount}টাকা',
+          icon: LineAwesomeIcons.dollar_sign,
+          color: const Color(0xffDF2DE3)),
+      HomeMenuModel(
+          title:
+              'বর্তমান মাসের মোট পাওনা\n(পরিমাণ/অর্থ)\n${pc.dashboardModel.value.currentMonthDueQuantity}কেজি/${pc.dashboardModel.value.currentMonthDueAmount}টাকা',
+          icon: LineAwesomeIcons.dollar_sign,
+          color: const Color(0xffFB6340)),
+      HomeMenuModel(
+          title: 'মোট কাস্টমার\n${pc.dashboardModel.value.totalCusdtomer}',
+          icon: LineAwesomeIcons.users,
+          color: const Color(0xffF5365C)),
+      HomeMenuModel(
+          title: 'মোট সাপ্লায়ার\n${pc.dashboardModel.value.totalSupplier}',
+          icon: LineAwesomeIcons.user_plus,
+          color: const Color(0xffCAD900))
+    ];
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<PublicController>(
-      builder: (pc) {
-        return Stack(
-          children: [
-            Scaffold(
-              appBar: AppBar(
-                title: Text('ড্যাশবোর্ড', style:StDecoration.boldTextStyle),
-                backgroundColor: AllColor.appBgColor,
-                elevation: 0.0,
-                titleSpacing: 0.0,
-                iconTheme: IconThemeData(color: Colors.grey.shade800),
-              ),
-              body: _bodyUI(pc),
+    return GetBuilder<PublicController>(builder: (pc) {
+      return Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: Text('ড্যাশবোর্ড', style: StDecoration.boldTextStyle),
+              backgroundColor: AllColor.appBgColor,
+              elevation: 0.0,
+              titleSpacing: 0.0,
+              iconTheme: IconThemeData(color: Colors.grey.shade800),
             ),
-            if(pc.loading.value) const LoadingWidget()
-          ],
-        );
-      }
-    );
+            body: _bodyUI(pc),
+          ),
+          if (pc.loading.value) const LoadingWidget()
+        ],
+      );
+    });
   }
 
-  Widget _bodyUI(PublicController pc)=>ListView(
-    physics: const BouncingScrollPhysics(),
-    children: [
-      ListView.separated(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        padding: EdgeInsets.symmetric(horizontal: dynamicSize(.04),vertical: dynamicSize(.02)),
-        itemCount: _dashboardDataList.length,
-        separatorBuilder: (context,index)=>SizedBox(height: dynamicSize(.04)),
-        itemBuilder: (context, index)=> DashboardTile(model: _dashboardDataList[index])
-      ),
-      SizedBox(height: dynamicSize(.04)),
+  Widget _bodyUI(PublicController pc) => ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(
+                  horizontal: dynamicSize(.04), vertical: dynamicSize(.02)),
+              itemCount: _dashboardDataList.length,
+              separatorBuilder: (context, index) =>
+                  SizedBox(height: dynamicSize(.04)),
+              itemBuilder: (context, index) =>
+                  DashboardTile(model: _dashboardDataList[index])),
+          SizedBox(height: dynamicSize(.04)),
 
-      ///Last 5 Transaction
-      Container(
-        padding: EdgeInsets.all(dynamicSize(.04)),
-        margin: EdgeInsets.symmetric(horizontal: dynamicSize(.04)),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(5))
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ///Header
-            Row(
+          ///Last 5 Transaction
+          Container(
+            padding: EdgeInsets.all(dynamicSize(.04)),
+            margin: EdgeInsets.symmetric(horizontal: dynamicSize(.04)),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(5))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(child: Text('শেষ ৫টি লেনদেন',style: StDecoration.boldTextStyle.copyWith(fontSize: dynamicSize(.05)))),
-                Icon(LineAwesomeIcons.angle_down,size: dynamicSize(.06))
+                ///Header
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text('শেষ ৫টি লেনদেন',
+                            style: StDecoration.boldTextStyle
+                                .copyWith(fontSize: dynamicSize(.05)))),
+                    Icon(LineAwesomeIcons.angle_down, size: dynamicSize(.06))
+                  ],
+                ),
+                const Divider(color: Colors.blueGrey, thickness: 1),
+
+                ///Transaction List
+                pc.fiveTransactionModel.value.data != null
+                    ? ListView.separated(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: pc.fiveTransactionModel.value.data!.length,
+                        separatorBuilder: (context, index) => const Divider(
+                            color: Colors.blueGrey, thickness: .5),
+                        itemBuilder: (context, index) => FiveTransactionTile(
+                            model: pc.fiveTransactionModel.value.data![index]))
+                    : Container()
               ],
             ),
-            const Divider(color: Colors.blueGrey,thickness: 1),
-
-            ///Transaction List
-            pc.fiveTransactionModel.value.data!=null
-                ? ListView.separated(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: pc.fiveTransactionModel.value.data!.length,
-                separatorBuilder: (context,index)=>const Divider(color: Colors.blueGrey,thickness: .5),
-                itemBuilder: (context, index)=> FiveTransactionTile(model: pc.fiveTransactionModel.value.data![index])
-            ) :Container()
-          ],
-        ),
-      ),
-      SizedBox(height: dynamicSize(.04)),
-    ],
-  );
+          ),
+          SizedBox(height: dynamicSize(.04)),
+        ],
+      );
 }
